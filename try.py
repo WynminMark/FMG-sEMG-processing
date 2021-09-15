efaf8e0adc566f7509499440eecc5778aa65b56f
WynminMark <651292793@qq.com>
2021-09-15 22:23:52 +0800

    210915

--- try.py
+++ try.py
@@ -4,14 +4,10 @@ Created on Tue Sep 14 14:40:28 2021
 
 @author: WeimyMark
 """
-import sqlite3
 
-conn = sqlite3.connect("D:\code\data\iFEMG/grade-0.db")
-cursor = conn.cursor()
-cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-Tables=cursor.fetchall()
-print(Tables)
-
+import pandas as pd
+data = pd.read_csv("D:\code\data\iFEMG\grade-0.db",sep=';')
+print(type(data))
 
 
 
