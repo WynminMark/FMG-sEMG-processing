2992e4cd4b768153399505e58c4b90e15de6ed71
WynminMark <651292793@qq.com>
2021-09-16 00:07:48 +0800

    210915

--- try.py
+++ try.py
@@ -6,8 +6,23 @@ Created on Tue Sep 14 14:40:28 2021
 """
 
 import pandas as pd
-data = pd.read_csv("D:\code\data\iFEMG\grade-0.db",sep=';')
-print(type(data))
+
+
+def pdcsv_read():
+    data = pd.read_csv("grade-0.db",sep=';')
+    print(type(data))
+
+def txt_read():
+    with open("grade-0.db") as f:
+        f.readlines()
+
+def pdtable_read():
+    data = pd.read_table("grade-0.db", sep = ';', header = None)
+    print(type(data))
+    print(data)
+
+if __name__ == '__main__':
+    pdtable_read()
 
 
 
