062dbe529a8a27bda49f3175bd9ff12e209b038c
WynminMark <651292793@qq.com>
2022-11-10 21:14:21 +0800

    multichannel model training

--- test.py
+++ test.py
@@ -1,12 +1,9 @@
-class A:
-    def hi(self):
-        self.a = 1
-
-class B(A):
-    def hi(self):
-        super().hi()         # 通过super调用父类A的hi()
-        self.a = 2
-        print(self.a)
-        
-b = B()
-b.hi()    # 这里调用的就是B里面的hi()
+import pandas as pd
+import numpy as np
+# groupby()
+df1 = pd.DataFrame({"col1":list("ababbc"),
+                   "col2":list("xxyyzz"),
+                   "number1":range(90,96),
+                   "number2":range(100,106)})
+df2 = df1.groupby(["col1","col2"]).agg({"number1":sum,
+                                        "number2":np.mean})
