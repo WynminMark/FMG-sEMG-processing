b44b70c3ec86bad96ccfbe653c7e673d86766291
WynminMark <651292793@qq.com>
2022-11-19 18:21:46 +0800

    little modified

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
 import pandas as pd
 import numpy as np
-# groupby()
+# groupby()多标签归类
 df1 = pd.DataFrame({"col1":list("ababbc"),
                    "col2":list("xxyyzz"),
                    "number1":range(90,96),
                    "number2":range(100,106)})
+print(df1)
 df2 = df1.groupby(["col1","col2"]).agg({"number1":sum,
                                         "number2":np.mean})
+
+print(df2)
